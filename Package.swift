d8ed812cebc47b38ed4a9c61a87ab6ef7d6e9bec
Pat Homelvig <pat@whitewolf.com>
2022-02-09 11:06:54 -0700

    Added test Package.swift

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "AppRTC",
+    platforms: [
+          .iOS(.v9),
+          .tvOS(.v9),
+          .macOS(.v10_11),
+    ],
+    products: [
+        .library(
+            name: "AppRTC",
+            targets: ["AppRTC"]),
+    ],
+    dependencies: [
+//        .package(url: "https://github.com/apple/swift-algorithms", from: "0.0.1")
+    ],
+    targets: [
+        .target(
+            name: "AppRTC",
+            dependencies: []
+//            dependencies: [.product(name: "Algorithms", package: "swift-algorithms")]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
